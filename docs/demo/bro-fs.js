32ec2a04e6e71df2f7d08fb377014807b071b478
vitalets <vitalets@yandex-team.ru>
2016-10-24 22:36:57 +0300

    docs

--- docs/demo/bro-fs.js
+++ docs/demo/bro-fs.js
@@ -1,4 +1,4 @@
-/*! bro-fs v0.1.5 */
+/*! bro-fs v0.1.10 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
